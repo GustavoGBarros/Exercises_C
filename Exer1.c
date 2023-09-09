3ae259d07b6f93f04c8040dcdd417116fd815173
GustavoGBarros <gustavogbarros01@gmail.com>
2023-09-09 16:56:17 -0300

    Exer1

--- Exer1.c
+++ Exer1.c
@@ -1,11 +1,7 @@
+// A
 #include <stdio.h>
 
-int main()
-{
-int IDADE, ANO;
-printf(" \n Informe o ao de nascimento: ");
-scanf("%i", &ANO);
-IDADE = 2023 - ANO;
-printf("Voc� tem %i anos e nasceu em %i anos", IDADE, ANO);
-return 0;
+int main (){
+    printf("\n Olá Mundo!!!");
+    return 0;
 }
